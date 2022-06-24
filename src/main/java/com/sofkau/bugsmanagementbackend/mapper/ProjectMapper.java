74cbc6aa4a7e1a9934fb41cb8f442d85cd27189a
Manuel Pineda <pineda.manuel@gmail.com>
2022-06-24 13:03:40 -0500

    ProjectMapper --- ProjectMapper implementation

--- src/main/java/com/sofkau/bugsmanagementbackend/mapper/ProjectMapper.java
+++ src/main/java/com/sofkau/bugsmanagementbackend/mapper/ProjectMapper.java
@@ -1,4 +1,24 @@
 package com.sofkau.bugsmanagementbackend.mapper;
 
+import com.sofkau.bugsmanagementbackend.collections.Project;
+import com.sofkau.bugsmanagementbackend.dtos.ProjectDTO;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
+@RequiredArgsConstructor
 public class ProjectMapper {
+
+    private final ModelMapper modelMapper;
+
+    public Function<Project, ProjectDTO> convertEntityToDto(){
+        return project -> modelMapper.map(project, ProjectDTO.class);
+    }
+
+    public Function<ProjectDTO, Project> convertDtoToEntity(){
+        return projectDTO -> modelMapper.map(projectDTO, Project.class);
+    }
 }
