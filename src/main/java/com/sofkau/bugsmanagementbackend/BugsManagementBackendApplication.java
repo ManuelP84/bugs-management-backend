f9902d3dce964684b745f37d14ea02520cf00605
Manuel Pineda <pineda.manuel@gmail.com>
2022-06-23 22:56:34 -0500

    Start project

---
+++ src/main/java/com/sofkau/bugsmanagementbackend/BugsManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sofkau.bugsmanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BugsManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugsManagementBackendApplication.class, args);
+	}
+
+}
