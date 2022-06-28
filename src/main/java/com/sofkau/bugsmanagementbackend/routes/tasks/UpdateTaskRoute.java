828a2f912553d7685ad7d5177adfa80f8d90ad06
Jehison Galarza <jehisonsc045@gmail.com>
2022-06-28 17:41:52 -0500

    Task: update tasks use case, route and test created and successfully tested

---
+++ src/main/java/com/sofkau/bugsmanagementbackend/routes/tasks/UpdateTaskRoute.java
@@ -0,0 +1,30 @@
+package com.sofkau.bugsmanagementbackend.routes.tasks;
+
+import com.sofkau.bugsmanagementbackend.dtos.TaskDTO;
+import com.sofkau.bugsmanagementbackend.usecases.tasks.UpdateTaskUseCase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+public class UpdateTaskRoute {
+
+    @Bean
+    public RouterFunction<ServerResponse> updateTaskRouter(UpdateTaskUseCase useCase){
+        return route(
+                POST("/v1/api/update/task").and(accept(MediaType.APPLICATION_JSON)),
+                request -> request.bodyToMono(TaskDTO.class)
+                        .flatMap(taskDto -> useCase.apply(taskDto))
+                        .flatMap(taskDto -> ServerResponse.status(HttpStatus.CREATED)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .bodyValue(taskDto))
+        );
+    }
+}
