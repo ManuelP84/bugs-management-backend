cb17ccc2195a3adbf4f8e2ef01edb6a8cee02d73
Jehison Galarza <jehisonsc045@gmail.com>
2022-06-29 13:38:09 -0500

    Bugs: collection, dto, mapper created; create bug use case, route created and successfully tested in postman, fix test pending

--- src/main/java/com/sofkau/bugsmanagementbackend/routes/bugs/CreateBugRoute.java
+++ src/main/java/com/sofkau/bugsmanagementbackend/routes/bugs/CreateBugRoute.java
@@ -1,4 +1,32 @@
 package com.sofkau.bugsmanagementbackend.routes.bugs;
 
+import com.sofkau.bugsmanagementbackend.dtos.BugsDTO;
+import com.sofkau.bugsmanagementbackend.dtos.TaskDTO;
+import com.sofkau.bugsmanagementbackend.usecases.bugs.CreateBugUseCase;
+import com.sofkau.bugsmanagementbackend.usecases.tasks.CreateTaskUseCase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
 public class CreateBugRoute {
+
+    @Bean
+    public RouterFunction<ServerResponse> createBugRouter(CreateBugUseCase useCase){
+        return route(
+                POST("/v1/api/save/bug").and(accept(MediaType.APPLICATION_JSON)),
+                request -> request.bodyToMono(BugsDTO.class)
+                        .flatMap(bugDto -> useCase.apply(bugDto))
+                        .flatMap(bugDto -> ServerResponse.status(HttpStatus.CREATED)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .bodyValue(bugDto))
+        );
+    }
 }
--- src/main/java/com/sofkau/bugsmanagementbackend/usecases/bugs/CreateBugUseCase.java
+++ src/main/java/com/sofkau/bugsmanagementbackend/usecases/bugs/CreateBugUseCase.java
@@ -1,4 +1,25 @@
 package com.sofkau.bugsmanagementbackend.usecases.bugs;
 
-public class CreateBugUseCase {
+import com.sofkau.bugsmanagementbackend.dtos.BugsDTO;
+import com.sofkau.bugsmanagementbackend.mapper.BugsMapper;
+import com.sofkau.bugsmanagementbackend.repository.IBugsRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Function;
+
+@Service
+@RequiredArgsConstructor
+public class CreateBugUseCase implements Function<BugsDTO, Mono<BugsDTO>> {
+
+    private final IBugsRepository repository;
+    private final BugsMapper mapper;
+
+    @Override
+    public Mono<BugsDTO> apply(BugsDTO bugDTO){
+        return repository
+                .save(mapper.convertDtoToEntity().apply(bugDTO))
+                .map(bugs -> mapper.convertEntityToDto().apply(bugs));
+    }
 }
